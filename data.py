52341c046512ac65c2de09818c125898ecfbda2d
GeorgeWu1204 <52784523+GeorgeWu1204@users.noreply.github.com>
2023-10-12 20:55:49 +0100

    data

---
+++ data.py
@@ -0,0 +1,15 @@
+import ast
+import utils
+
+# Read the file
+def read_from_data(file_name):
+    with open(file_name, 'r') as f:
+        content = f.read()
+        raw_data = ast.literal_eval(content)
+        data_set = utils.Data_Set(raw_data)
+    return data_set
+
+
+if __name__ == '__main__':
+    data_set = read_from_data('ppa.txt')
+    print(data_set.list_all_contraints())
---
+++ utils.py
@@ -0,0 +1,34 @@
+
+class Data_Sample:
+    def __init__(self, data):
+        self.Constraints = data[0]
+        self.Cycle_count = data[1]
+        self.PPA = data[2]
+    def print_constraints(self):
+        print("Constraints:")
+        for k, v in self.Constraints.items():
+            print(f"{k} = {v}")
+
+    def print_cycle_count(self):
+        print("Cycle count:")
+        for k, v in self.Cycle_count.items():
+            print(f"{k} = {v}")
+    
+    def print_ppa(self):
+        print("PPA:")
+        for k, v in self.PPA.items():
+            print(f"{k} = {v}")
+
+class Data_Set:
+    def __init__(self, data):
+        for i in range(len(data)):
+            self.__dict__[i] = Data_Sample(data[i])
+    
+    def __len__(self):
+        return len(self.__dict__)
+    
+    def list_all_contraints(self):
+        constraints = []
+        for i in range(len(self)):
+            constraints.append(self.__dict__.get(i).Constraints)
+        return constraints
